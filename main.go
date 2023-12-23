1bf6a04d72c5c7e2f44a267588db968f95c7c965
127.0.0.1 <sacredspirits47@gmail.com>
2023-12-23 17:56:36 +0530

    basics of go lang are added, static site didnt work

--- main.go
+++ main.go
@@ -1,39 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func processHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/process" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintf(w, "Hello!")
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful")
-
-}
+import "fmt"
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/process", processHandler)
-	fmt.Println("Starting server at port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	//Lets create a user input of size 10 array
+	var size int
+	fmt.Println("Enter the size of the array")
+	fmt.Scan(&size)
+	//make is used to create a slice of size 10 for dynamic array
+	arr := make([]int, size)
+	fmt.Println("Enter the elements of the array")
+	for i := 0; i < size; i++ {
+		fmt.Scan(&arr[i])
+	}
+	fmt.Println("The elements of the array are")
+	for i := 0; i < size; i++ {
+		fmt.Println(arr[i])
 	}
 }
